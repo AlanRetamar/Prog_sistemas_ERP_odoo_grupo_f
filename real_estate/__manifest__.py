39cdd1f9da814e666f1b434b082f68fbb3f5c0b1
Alan <alanretamar99@outlook.com.ar>
2025-08-25 17:40:28 -0300

    primer commit en dev

---
+++ real_estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Inmobiliaria',
+    'version': '15.0.1.0.0',
+    'summary': 'Módulo de prueba para inmobiliaria',
+    'category': 'Unla',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
