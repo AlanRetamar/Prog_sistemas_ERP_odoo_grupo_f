aae188cca152d2820b293399def67524aeb01683
Alan <alanretamar99@outlook.com.ar>
2025-08-27 03:48:54 -0300

    Añado init y modifico manifest

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -1,10 +1,16 @@
+# Punto 2
 {
     'name': 'Inmobiliaria',
     'version': '15.0.1.0.0',
     'summary': 'Módulo de prueba para inmobiliaria',
     'category': 'Unla',
     'depends': ['base'],
-    'data': [],
+    'data': [
+        'security/real_estate_res_groups.xml', # Punto 12
+        'security/ir.model.access.csv', # Punto 10
+        'views/estate_property_views.xml', # Punto 5 y 17
+        'views/real_estate_menuitem.xml' # Punto 6
+    ],
     'installable': True,
     'application': True,
 }
