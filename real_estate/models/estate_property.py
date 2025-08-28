2fbe62fc6a3defdbd43ec5fe04b80d59821aa0de
Dylan Thomas Lopez <dylopez@smn.gob.ar>
2025-08-28 14:59:10 -0300

    security and models

---
+++ real_estate/models/estate_property.py
@@ -0,0 +1,31 @@
+from odoo import models, fields
+
+# hereda de Model
+class EstateProperty(models.Model): 
+    _name = 'estate.property'
+    # la description es optativa, pero la ponemos
+    _description = 'Propiedades'
+
+    #creamos campos
+    name = fields.Char(string="Título", required=True)
+    description = fields.Text(string="Descripción")
+    postcode = fields.Char(string="Código Postal")
+    date_availability = fields.Date(string="Fecha disponibilidad")
+    expected_price = fields.Float(string="Precio esperado")
+    selling_price = fields.Float(string="Precio de venta")
+    bedrooms = fields.Integer(string="Habitaciones",
+                            default=2)
+    living_area = fields.Integer(string="Superficie cubierta")
+    facades = fields.Integer(string="Fachadas")
+    garage = fields.Boolean(string="Garage")
+    garden = fields.Boolean(string="Jardín")
+    garden_orientation = fields.Selection(selection=[
+                                            ('north', 'Norte'),
+                                            ('south', 'Sur'),
+                                            ('east', 'Este'),
+                                            ('west', 'Oeste'),
+                                            ],
+                                            default="north",
+                                            string="Orientación del jardín"
+                                        )
+    garden_area = fields.Integer(string="Superficie jardín")
