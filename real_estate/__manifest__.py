86059efc98da821a7570e6e8f397c5738d9f8bdb
Alan <alanretamar99@outlook.com.ar>
2025-09-16 23:24:06 -0300

    Modifico el manifest

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -11,10 +11,10 @@
         'security/real_estate_res_groups.xml', # Punto 12
         'security/ir.model.access.csv', # Punto 10
         'views/estate_property_views.xml', # Punto 5 y 17
-        'views/estate_menu_item.xml' # Punto 6
         'views/estate_property_type_views.xml', # Punto 27
         'views/estate_property_tag_views.xml', # Punto 27
-        'views/real_estate_menuitem.xml' # Punto 6
+        'views/estate_property_offer_views.xml', # Punto 40
+        'views/real_estate_menuitem.xml', # Punto 6
     ],
     'application': True,
 }
