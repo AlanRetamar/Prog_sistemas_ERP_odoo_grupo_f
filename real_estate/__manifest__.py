7c660291b22b9e31d5a39da8c05b91e3e9b93d46
Alan <alanretamar99@outlook.com.ar>
2025-09-16 23:30:58 -0300

    Modifico init y manifest

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -11,8 +11,8 @@
         'security/real_estate_res_groups.xml', # Punto 12
         'security/ir.model.access.csv', # Punto 10
         'views/estate_property_views.xml', # Punto 5 y 17
-        'views/estate_property_type_views.xml', # Punto 27
-        'views/estate_property_tag_views.xml', # Punto 27
+        'views/estate_property_type_views.xml', # Punto 27 y 33
+        'views/estate_property_tag_views.xml', # Punto 33
         'views/estate_property_offer_views.xml', # Punto 40
         'views/real_estate_menuitem.xml', # Punto 6
     ],
