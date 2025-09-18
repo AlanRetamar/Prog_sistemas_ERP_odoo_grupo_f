64592269ffb328629a972bb2f9d7f30f7ca682c8
Alan <alanretamar99@outlook.com.ar>
2025-09-18 19:30:32 -0300

    Modifico modelos y vistas

--- real_estate/models/estate_property_offer.py
+++ real_estate/models/estate_property_offer.py
@@ -1,17 +1,27 @@
 from odoo import models, fields
-
-# ---- punto 37
-
-# hereda de Model
-class EstatePropertOffer(models.Model): 
-    _name = 'estate.property.offer'
-    _description = 'Oferta sobre propiedad'
-
-    #campos
+# Punto 37
+class EstatePropertyOffer(models.Model):
+    _name= 'estate.property.offer'
+    _description = 'Oferta sobre Propiedad'
+    
     price = fields.Float(string="Precio", required=True)
-    status = fields.Selection(string="Estado", selection=[
-                                                    ("accepted", "Aceptada"),
-                                                    ("refused", "Rechazada")
-                                                ])
-    partner_id = fields.Many2one(comodel_name= "res.partner", string="Ofertante", required=True)
-    property_id = fields.Many2one(comodel_name= "estate.property", string="Propiedad", required=True)
+    
+    status = fields.Selection(
+        selection=[
+            ('accepted', 'Aceptada'),
+            ('refused', 'Rechazada'),
+        ]
+    )
+    
+    partner_id = fields.Many2one(
+        comodel_name = 'res.partner',
+        string = 'Ofertante',
+        required = True,
+    )
+    
+    property_id = fields.Many2one(
+        comodel_name = 'estate.property',
+        string = 'Propiedad',
+        required = True,
+    )
+    
